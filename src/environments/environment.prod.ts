e4c4280188f39063767499429777fd7be8d77b2f
Anthony Tang <anthony.tang2@ucalgary.ca>
2021-02-14 22:42:15 -0700

    Pushing to gh pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
+  production: false,
   firebase: {
     apiKey: "AIzaSyBIS3dVUxDR7iNa_1WLSpltw73AOwRuX90",
     authDomain: "mysite-a9c75.firebaseapp.com",
@@ -9,5 +9,11 @@ export const environment = {
     messagingSenderId: "1024630662056",
     appId: "1:1024630662056:web:dd23e895d8817e3feeb39d",
     measurementId: "G-3NHP8GFPSH"
+  },
+  cloudrun: {
+    host: "https://mysite-server-upas5adfra-uw.a.run.app",
+    paths: {
+      fetchIp: "/fetch-ip"
+    }
   }
 };
