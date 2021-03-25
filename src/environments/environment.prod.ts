894c34b39fe1d53e581f35ad69f62c076517315f
Anthony <a.t.1592653@gmail.com>
2021-03-25 04:01:15 -0600

    Pushing to GH Pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,15 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyBIS3dVUxDR7iNa_1WLSpltw73AOwRuX90",
-    authDomain: "mysite-a9c75.firebaseapp.com",
-    databaseURL: "https://mysite-a9c75-default-rtdb.firebaseio.com",
-    projectId: "mysite-a9c75",
-    storageBucket: "mysite-a9c75.appspot.com",
-    messagingSenderId: "1024630662056",
-    appId: "1:1024630662056:web:dd23e895d8817e3feeb39d",
-    measurementId: "G-3NHP8GFPSH"
+    // TODO ANTHONY This should be changed
+    apiKey: "AIzaSyAXxHlIkHXYfKJkUiUlPdpXd5pSjxAIfOM",
+    authDomain: "mysite-b69a0.firebaseapp.com",
+    databaseURL: "https://mysite-b69a0-default-rtdb.firebaseio.com",
+    projectId: "mysite-b69a0",
+    storageBucket: "mysite-b69a0.appspot.com",
+    messagingSenderId: "535138359470",
+    appId: "1:535138359470:web:48c88f22341badd447353f",
+    measurementId: "G-VDH4BDDHQQ"
   },
   cloudrun: {
     host: "https://mysite-server-upas5adfra-uw.a.run.app",
